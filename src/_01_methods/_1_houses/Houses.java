37c233976ae0cf5973f71eb9d42af7e92844b368
league <league@wts14-3.attlocal.net>
2024-11-23 14:29:07 -0800

    max is great

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,11 +1,37 @@
 package _01_methods._1_houses;
+
+import org.jointheleague.graphical.robot.Robot;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
  */
+
 public class Houses {
+	Robot rob;
 		
 	public void run() {
+		
 		// Check the recipe to find out what code to put here
+		rob = new Robot();
+		rob.penDown();
+		rob.setSpeed(9);
+		rob.moveTo(50, 550);
+		rob.turn(90);
+		drawTriangleHouse(50);
 	}
+	
+	public void drawTriangleHouse(int height) {
+		Random random = new Random();
+		rob.setPenColor(1, 20, 1);
+		rob.move(30);
+		rob.turn(-90);
+		rob.move(height);
+		
+		
+
+		
+		
+	}
+	
 }
